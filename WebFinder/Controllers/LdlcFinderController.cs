2900bfcba8c2519c31e545121fbb22ac7ea08d09
Quentin <stagiaire63@rtl.be>
2022-01-23 21:08:21 +0100

    Create Product model Get name is working

--- WebFinder/Controllers/LdlcFinderController.cs
+++ WebFinder/Controllers/LdlcFinderController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using WebFinder.Models;
 
 namespace WebFinder.Controllers;
 
@@ -8,15 +9,36 @@ namespace WebFinder.Controllers;
 public class LdlcFinderController : ControllerBase
 {
     [HttpGet(Name = "GetLdlcSourceCode")]
-    public string Get()
+    public List<Product> Get()
     {
         string htmlCode = GetSourceCode("https://www.ldlc.com/fr-be/recherche/rtx%203060/");
 
-        htmlCode = RemoveBefore(htmlCode, "listing-product");
+        htmlCode = RemoveBefore(htmlCode, "class=\"pdt-item\"");
 
         htmlCode = RemoveAfter(htmlCode, "pagination");
 
-        return htmlCode;
+        string[] htmlElements = GetSplittedElements(htmlCode, "class=\"pdt-item\"");
+
+        List<Product> products = new List<Product>();
+        foreach (string element in htmlElements)
+        {
+            int pFrom = element.IndexOf("<h3 class =\"title-3\">") + "<h3 class =\"title-3\">".Length;
+            int pTo = element.LastIndexOf("</a></h3>");
+
+            string result = element.Substring(pFrom, pTo - pFrom);
+            result = RemoveBefore(result, ".html\">");
+            products.Add(new Product
+            {
+                Name = result
+            });
+        }
+
+        return products;
+    }
+
+    private string[] GetSplittedElements(string htmlCode, string splitString)
+    {
+        return htmlCode.Split(new[] { splitString }, StringSplitOptions.RemoveEmptyEntries);
     }
 
     private string GetSourceCode(string url)
@@ -32,7 +54,7 @@ public class LdlcFinderController : ControllerBase
 
     private string RemoveBefore(string htmlCode, string stringToFind)
     {
-        return htmlCode.Substring(htmlCode.IndexOf(stringToFind));
+        return htmlCode.Substring(htmlCode.IndexOf(stringToFind) + stringToFind.Length);
     }
 
     private string RemoveAfter(string htmlCode, string stringToFind)
---
+++ WebFinder/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebFinder.Models
+{
+    public class Product
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
