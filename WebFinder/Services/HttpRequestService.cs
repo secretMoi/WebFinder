380abdd2e87fe7dd33c269712e75b2dcd28e4a97
Lampirou <win.moi@hotmail.com>
2022-02-05 23:26:00 +0100

    Add discord bot sending private message

--- WebFinder/Services/HttpRequestService.cs
+++ WebFinder/Services/HttpRequestService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using Discord;
+using Discord.WebSocket;
 using WebFinder.Controllers;
 
 namespace WebFinder.Services;
@@ -7,6 +9,7 @@ public class HttpRequestService : BackgroundService
 {
     private readonly ILogger<HttpRequestService> _logger;
     private readonly IServiceScopeFactory  _serviceScopeFactory;
+    private DiscordSocketClient  _client;
 
     public HttpRequestService(ILogger<HttpRequestService> logger, IServiceScopeFactory  serviceScopeFactory)
     {
@@ -25,6 +28,19 @@ public class HttpRequestService : BackgroundService
             {
                 // foreach(var product in await ldlcService.GetProducts("rtx 3090"))
                 //     _logger.LogInformation($"[{DateTime.Now}] {product}");
+                
+                _client = new DiscordSocketClient();
+                _client.Log += Log;
+                _client.Connected += ClientOnConnected;
+                
+                var token = "";
+                // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+                // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+                // var token = File.ReadAllText("token.txt");
+                // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
             }
             catch (Exception ex)
             {
@@ -34,5 +50,19 @@ public class HttpRequestService : BackgroundService
 
             await Task.Delay(30000, cancellationToken);
         }
+    }
+
+    private async Task ClientOnConnected()
+    {
+        var user = await _client.GetUserAsync(0);
+        var channel = await user.CreateDMChannelAsync();
+        await channel.SendMessageAsync("coucou");
+    }
+
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
